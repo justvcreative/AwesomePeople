e7319e54c75c93d1cf46fa8540dfd86490ebeccf
justvcreative <justvcreative@gmail.com>
2017-03-19 19:52:13 -0400

    CRUD works

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -3,20 +3,20 @@
 //App Controller
 angular.module("awesomeapp")
 .controller('mainCtrl', function($scope, $http, dataService){
-    
     //CREATE & Update
     $scope.savePerson = (person) => {
         dataService.savePerson(person);
     }
     
     //READ
-    dataService.getPeople(function(res) {
-        $scope.people = res.data;
+    dataService.getPeople( (res) => {
+        let people = res.data
+        $scope.people = people;
     });
     
     //DELETE
     $scope.deletePerson = (person, $index) => {
-        dataService.deletePeople(person);
+        dataService.deletePerson(person);
         $scope.people.splice($index, 1);
     }
 })
--- public/js/services/app.service.js
+++ public/js/services/app.service.js
@@ -2,37 +2,30 @@
 
 // App Service
 angular.module("awesomeapp")
-.service('dataService', function($http, $q){
+.service('dataService', function($http){
     
     //CREATE & UPDATE
-    this.savePerson = (people) => {
-        let queue = [];
-          people.forEach(function(person) {
-              let req;
-              //if it does not have an ID
-              if(!person._id) {
-                  //create a new person (post)
-                  req = $http.post('/api', person);
-              } else {
-                  //if it does have an ID, update existing person (put)
-                  req = $http.put('/api/' + person._id, person).then(function(result){
-                      person = result.data.person;
-                      return person;
-                  });
-              };
-              queue.push(req);
-          });
-          return $q.all(queue).then(function(results){
-              console.log('I saved ' + people.length + ' people!');
-          });
+    this.savePerson = (person) => {
+        let req;
+        //if it does not have an ID
+        if(!person._id) {
+            //create a new person (post)
+            req = $http.post('/api', person);
+        } else {
+            //if it does have an ID, update existing person (put)
+            req = $http.put('/api/' + person._id, person).then(function(result){
+                person = result.data.person;
+                return person;
+            });
+        };
       };
     
     //READ
     this.getPeople = (cb) => {$http.get('/api').then(cb) };
     
     //DELETE
-    this.deletePerson = (person) => { 
-        console.log(person.name + ' was deleted'); 
+    this.deletePerson = (person) => {
+        $http.delete('/api/' + person._id).then(person);
     };
 
 })
